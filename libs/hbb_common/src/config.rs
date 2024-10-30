1c4f071685e0c02e7f09fd73f6e9d316c6f93774
a33356mm <63138898+a33356mm@users.noreply.github.com>
2024-10-30 21:05:11 +0800

    config.rs

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -100,8 +100,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const PUBLIC_RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["kr.33356.gq"];
+pub const PUBLIC_RS_PUB_KEY: &str = "OCc9qwzsRKn2+jPEvQYDFXv4do2eXc0ee7iAmBwbtGk=";
 
 pub const RS_PUB_KEY: &str = match option_env!("RS_PUB_KEY") {
     Some(key) if !key.is_empty() => key,
